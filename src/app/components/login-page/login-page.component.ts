374edeffab767f18b758d70d077da649cc84b2ae
amanmoon <125746010+amanmoon@users.noreply.github.com>
2023-03-02 01:23:12 +0530

    error

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { config } from '../../config';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  loginData: {userinput : any, passwordInput:any,id?: string }
+  constructor(public http: HttpClient) { } 
+  postLoginData(loginData: {userinput : any, passwordInput:any,id?: string }) {
+    // const headers = new HttpHeaders({ myHeader: 'Angular' });
+    const httpOptions = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'*',
+        'Authorization':'authkey',
+        'userid':'1'
+      })
+    };
 
+    this.http
+      .post<{ name: string }>(
+        'https://testserver-768e5-default-rtdb.firebaseio.com/data.json',
+        loginData
+      )
+      .subscribe((ref) => { });
+  }
+    
+    public fetchLoginData() {
+      this.http
+        .get<{
+          [key: string]: { userinput : any, passwordInput:any,id?: string  };
+        }>(config.url+'registere/?format=json')
+        .pipe(
+          map((ref) => {
+            const data = [];
+            for (const key in ref) {
+              if (ref.hasOwnProperty(key)) {
+                data.push({ ...ref[key], id: key });
+              }
+            }
+            return data;
+          })
+        )
+        .subscribe((data) => {
+          // if () {}
+          // else{}
+          console.log(data)
+    });
+    }
 }
