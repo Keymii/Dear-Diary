8785543f79ceeb2a5135c10541de5a6df4650807
Akshat <22b0690@iitb.ac.in>
2023-03-02 11:50:23 +0530

    submit button corrected , just endpoints needed to be added . now only after validation only whole progress bar will be shown

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { AppComponent } from './app.component';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Router } from '@angular/router';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { TextAreaComponent } from './components/text-area/text-area.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
-const routes: Routes = [
+const routes: Routes = [ {path: '', component:LoginPageComponent},
   { path: 'note', component:TextAreaComponent  },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterUserComponent },
